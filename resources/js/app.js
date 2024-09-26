fc73da94d52de1dcf686b583d92d1edded17056d
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-09-26 21:04:52 +0800

    fixed the issue on custom layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,15 @@
-import './bootstrap';
-import '../css/app.css';
-import Vue3Toastify from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
-import { createNotivue } from 'notivue'
-import 'notivue/notification.css' // Only needed if using built-in notifications
-import 'notivue/animations.css' // Only needed if using built-in animations
-import 'notivue/notification-progress.css'
+import "./bootstrap";
+import "../css/app.css";
+import Vue3Toastify from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
+import { createNotivue } from "notivue";
+import "notivue/notification.css"; // Only needed if using built-in notifications
+import "notivue/animations.css"; // Only needed if using built-in animations
+import "notivue/notification-progress.css";
 
-
-import { createApp, h } from 'vue';
-import {createInertiaApp, Link} from '@inertiajs/vue3';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { createApp, h } from "vue";
+import { createInertiaApp, Link } from "@inertiajs/vue3";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
 import MainLayout from "@/Layouts/MainLayout.vue";
 
 const notivue = createNotivue({
@@ -18,35 +17,36 @@ const notivue = createNotivue({
     avoidDuplicates: true,
     notifications: {
         global: {
-            duration: 3000
-        }
-    }
+            duration: 3000,
+        },
+    },
 });
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        let page = pages[`./Pages/${name}.vue`]
-        page.default.layout = MainLayout
-        return page
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        let page = pages[`./Pages/${name}.vue`];
+        if (!page.default.layout) {
+            page.default.layout = MainLayout;
+        }
+        return page;
     },
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
             .use(Vue3Toastify, {
-                position: 'top-center',
+                position: "top-center",
                 autoClose: 2000,
-                closeButton: false
+                closeButton: false,
             })
             .use(notivue)
-            .component('Link', Link)
+            .component("Link", Link)
             .mount(el);
     },
     progress: {
-        color: '#7152F3',
+        color: "#7152F3",
     },
-
 });
